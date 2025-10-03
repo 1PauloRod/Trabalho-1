168560ef791ae879acf4fd37c4e8496e0f827498
Paulo Rodrigues <pauloalvinegrodecoracao@hotmail.com>
2025-10-03 20:36:21 -0300

    ajustes finais

--- biblioteca_digital/settings.py
+++ biblioteca_digital/settings.py
@@ -112,9 +112,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+# Para desenvolvimento: pasta com seus arquivos estáticos customizados
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+# Para produção: pasta para onde o collectstatic vai copiar tudo
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
